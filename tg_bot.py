b7488be1f14aff207eef015eee37e9933234d95e
tram888 <bigtram888@gmail.com>
2023-06-30 16:22:09 +0900

    Adding partial functions to the functools module

--- tg_bot.py
+++ tg_bot.py
@@ -3,6 +3,7 @@ import logging
 import os
 import pathlib
 import random
+from functools import partial
 from logging.handlers import RotatingFileHandler
 
 import redis
@@ -14,8 +15,90 @@ from telegram.ext import CallbackContext, CommandHandler, Updater, CallbackQuery
 
 from get_questions_answers_script import get_questions_answers
 
-
 logger = logging.getLogger(__name__)
+
+START = 1
+
+
+def error_handler(update: Update, context: CallbackContext):
+    logger.exception(msg="Произошло исключение", exc_info=context.error)
+
+
+def start(update: Update, context: CallbackContext, redis_db):
+    redis_db.set('user_id', update.effective_chat.id)
+    context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text='<b>🤜Привет я бот для викторин!🤛</b>',
+        parse_mode=telegram.ParseMode.HTML,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton(text='Новый вопрос', callback_data='new_question')],
+                [InlineKeyboardButton(text='Мой счет', callback_data='my_score')],
+            ]
+        )
+    )
+    return ConversationHandler.END
+
+
+def new_question(update: Update, context: CallbackContext, quiz_info, redis_db):
+    random_question_answer = random.choice(list(quiz_info.items()))
+    question = random_question_answer[0]
+    redis_db.set('question', question)
+    redis_db.set('answer', random_question_answer[1])
+    update.callback_query.edit_message_text(question, parse_mode=telegram.ParseMode.HTML,
+                                            reply_markup=InlineKeyboardMarkup(
+                                                [
+                                                    [InlineKeyboardButton(text='Сдаться',
+                                                                          callback_data='give_up')],
+                                                    [InlineKeyboardButton(text='Мой счет',
+                                                                          callback_data='my_score')],
+                                                ]
+                                            ))
+    return START
+
+
+def give_up(update: Update, context: CallbackContext, redis_db):
+    full_answer = redis_db.get('answer').decode('utf-8')
+    update.callback_query.edit_message_text(f'Правильный ответ: {full_answer}',
+                                            reply_markup=InlineKeyboardMarkup(
+                                                [
+                                                    [InlineKeyboardButton(text='Новый вопрос',
+                                                                          callback_data='new_question')],
+                                                    [InlineKeyboardButton(text='Мой счет',
+                                                                          callback_data='my_score')],
+                                                ]
+                                            ))
+    return ConversationHandler.END
+
+
+def answer(update: Update, context: CallbackContext, redis_db):
+    full_answer = redis_db.get('answer').decode('utf-8')
+    end_index = min(full_answer.find('('), full_answer.find('.'))
+    short_answer = full_answer[:end_index].strip().lower()
+    if update.message.text.strip().lower() == short_answer:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='🎉Правильно! Поздравляю! Ты молодец!🎉',
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton(text='Новый вопрос', callback_data='new_question'),
+                     InlineKeyboardButton(text='Сдаться', callback_data='give_up')],
+                    [InlineKeyboardButton(text='Мой счет', callback_data='my_score')],
+                ]
+            )
+        )
+        return ConversationHandler.END
+    else:
+        context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='👎Не правильно! Попробуй еще раз!👎',
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [InlineKeyboardButton(text='Сдаться', callback_data='give_up')],
+                    [InlineKeyboardButton(text='Мой счет', callback_data='my_score')],
+                ]
+            )
+        )
 
 
 def main():
@@ -47,101 +130,28 @@ def main():
     )
     args = parser.parse_args()
     file_path = args.file_path
-    quiz = get_questions_answers(filepath=file_path)
+    questions_answers = get_questions_answers(filepath=file_path)
+
+    partial_start = partial(start, redis_db=redis_db)
+    partial_new_question = partial(new_question, redis_db=redis_db, quiz_info=questions_answers)
+    partial_give_up = partial(give_up, redis_db=redis_db)
+    partial_answer = partial(answer, redis_db=redis_db)
 
     updater = Updater(bot_token, use_context=True)
     dp = updater.dispatcher
 
-    START = 1
-
-    def error_handler(update: Update, context: CallbackContext):
-        logger.exception(msg="Произошло исключение", exc_info=context.error)
-
-    def start(update: Update, context: CallbackContext):
-        redis_db.set('user_id', update.effective_chat.id)
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text='<b>🤜Привет я бот для викторин!🤛</b>',
-            parse_mode=telegram.ParseMode.HTML,
-            reply_markup=InlineKeyboardMarkup(
-                [
-                    [InlineKeyboardButton(text='Новый вопрос', callback_data='new_question')],
-                    [InlineKeyboardButton(text='Мой счет', callback_data='my_score')],
-                ]
-            )
-        )
-        return ConversationHandler.END
-
-    def new_question(update: Update, context: CallbackContext):
-        random_question_answer = random.choice(list(quiz.items()))
-        question = random_question_answer[0]
-        redis_db.set('question', question)
-        redis_db.set('answer', random_question_answer[1])
-        update.callback_query.edit_message_text(question, parse_mode=telegram.ParseMode.HTML,
-                                                reply_markup=InlineKeyboardMarkup(
-                                                    [
-                                                        [InlineKeyboardButton(text='Сдаться',
-                                                                              callback_data='give_up')],
-                                                        [InlineKeyboardButton(text='Мой счет',
-                                                                              callback_data='my_score')],
-                                                    ]
-                                                ))
-        return START
-
-    def give_up(update: Update, context: CallbackContext):
-        full_answer = redis_db.get('answer').decode('utf-8')
-        update.callback_query.edit_message_text(f'Правильный ответ: {full_answer}',
-                                                reply_markup=InlineKeyboardMarkup(
-                                                    [
-                                                        [InlineKeyboardButton(text='Новый вопрос',
-                                                                              callback_data='new_question')],
-                                                        [InlineKeyboardButton(text='Мой счет',
-                                                                              callback_data='my_score')],
-                                                    ]
-                                                ))
-        return ConversationHandler.END
-
-    def answer(update: Update, context: CallbackContext):
-        full_answer = redis_db.get('answer').decode('utf-8')
-        end_index = min(full_answer.find('('), full_answer.find('.'))
-        short_answer = full_answer[:end_index].strip().lower()
-        if update.message.text.strip().lower() == short_answer:
-            context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text='🎉Правильно! Поздравляю! Ты молодец!🎉',
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [InlineKeyboardButton(text='Новый вопрос', callback_data='new_question'),
-                         InlineKeyboardButton(text='Сдаться', callback_data='give_up')],
-                        [InlineKeyboardButton(text='Мой счет', callback_data='my_score')],
-                    ]
-                )
-            )
-            return ConversationHandler.END
-        else:
-            context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text='👎Не правильно! Попробуй еще раз!👎',
-                reply_markup=InlineKeyboardMarkup(
-                    [
-                        [InlineKeyboardButton(text='Сдаться', callback_data='give_up')],
-                        [InlineKeyboardButton(text='Мой счет', callback_data='my_score')],
-                    ]
-                )
-            )
-
     conv_handler = ConversationHandler(
-        entry_points=[CallbackQueryHandler(new_question, pattern='new_question')],
+        entry_points=[CallbackQueryHandler(partial_new_question, pattern='new_question')],
         states={
             START: [
-                MessageHandler(Filters.text & (~Filters.command), answer),
+                MessageHandler(Filters.text & (~Filters.command), partial_answer),
             ],
         },
-        fallbacks=[CommandHandler("start", start), CallbackQueryHandler(give_up, pattern='give_up')],
+        fallbacks=[CommandHandler("start", start), CallbackQueryHandler(partial_give_up, pattern='give_up')],
     )
 
     dp.add_handler(conv_handler)
-    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("start", partial_start))
 
     dp.add_error_handler(error_handler)
 
